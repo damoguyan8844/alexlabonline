6df9a78e56ca71afd99ba4499db874234aa36737
damoguyan8844 <damoguyan8844@3a4e9f68-f5c2-36dc-e45a-441593085838>
2010-08-31 12:30:15 +0000

    Support auto hide after one second

--- trunk/DaemonManager/Resource.h
+++ trunk/DaemonManager/Resource.h
@@ -4,6 +4,7 @@
 //
 #define IDD_DAEMONMANAGER_DIALOG        102
 #define IDR_MAINFRAME                   128
+#define IDC_BUTTON_FIX                  1001
 #define IDC_CURSOR                      1002
 #define IDC_TIMER                       1003
 
@@ -13,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
