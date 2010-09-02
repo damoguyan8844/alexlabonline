1c6e1946fa30cc4acee6dc1c82dc87b13d01e1af
damoguyan8844 <damoguyan8844@3a4e9f68-f5c2-36dc-e45a-441593085838>
2010-09-02 01:13:42 +0000

    ADD SQLiteBrowser Project

---
+++ SQLiteBrowser/trunk/sqlitebrowser/sqlitebrowser/extendedmainform.h
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------
+// ExtendedMainForm: This class extends the mainForm class
+//                       to support drag and drop of sql file.
+//
+// Developer:        Arvind Kumar Gupta
+//                   arvind.your@gmail.com
+//---------------------------------------------------------------
+
+#ifndef __EXTENDEDMAINFORM__
+#define	__EXTENDEDMAINFORM__
+
+#include "form1.h"
+#include <QDragEnterEvent>
+
+class ExtendedMainForm : public mainForm
+{
+public:
+	ExtendedMainForm(QWidget* parent = 0, const char* name = 0, Qt::WindowFlags fl = Qt::WType_TopLevel);
+	~ExtendedMainForm(void);
+
+protected:
+	void dragEnterEvent(QDragEnterEvent *event);
+	void dropEvent(QDropEvent *event);
+};
+
+#endif // __EXTENDEDMAINFORM__
+
